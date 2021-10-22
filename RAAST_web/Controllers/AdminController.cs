5c2bb1218505b305691b5d5fee29f1109922a7d3
Yavuz Uca <yavuz_uca@hotmail.com>
2021-10-22 17:13:18 +0200

    Added navbar for Admin and form to add editors

---
+++ RAAST_web/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RAAST_web.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Editor
+        public ActionResult AddEditor()
+        {
+            return View();
+        }
+        public ActionResult Verify()
+        {
+            return View("Verify");
+        }
+    }
+}
