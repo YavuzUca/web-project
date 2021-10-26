e4dcbc5f607a6a2a58e697f39b1300106a08bbf6
Yannick <yannickvreede@gmail.com>
2021-10-26 15:40:09 +0200

    New page for editors

---
+++ RAAST_web/Controllers/EditorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RAAST_web.Controllers
+{
+    public class EditorController : Controller
+    {
+        // GET: Editor
+        public ActionResult AddPost()
+        {
+            return View();
+        }
+        public ActionResult Verify()
+        {
+            return View("Verify");
+        }
+    }
+}
