ac39e90b0a451a2584c0a8ca4b248bcbba4d6656
Yannick <yannickvreede@gmail.com>
2021-11-15 14:43:49 +0100

    add comments

--- RAAST_web/Models/Editor.cs
+++ RAAST_web/Models/Editor.cs
@@ -5,9 +5,13 @@ using System.Web;
 
 namespace RAAST_web.Models
 {
+    /// <summary>
+    /// editor class
+    /// </summary>
     public class Editor
     {
         private string userName { get; set; }
         private string passWord { get; set; }
+        private string fullName { get; set; }
     }
 }
