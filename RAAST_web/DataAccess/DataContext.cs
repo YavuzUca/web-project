7119ac70108a0634c7407b2c4e940863e43aef5e
Yannick <yannickvreede@gmail.com>
2021-11-17 09:26:41 +0100

    added necessary packages and data-access folder

---
+++ RAAST_web/DataAccess/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RAAST_web.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace RAAST_web.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("Datacontext")
+        {
+
+        } 
+
+        public DbSet<Editor> editors { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
