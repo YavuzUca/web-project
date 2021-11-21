d21b92553275f40d63f1f75e6b0922cad3cbbdef
Anpan <hsekimonyo@gmail.com>
2021-11-21 13:17:44 +0100

    SMTPPP

--- RAAST_web/Controllers/EditorController.cs
+++ RAAST_web/Controllers/EditorController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using RAAST_web.Models;
@@ -21,6 +22,33 @@ namespace RAAST_web.Controllers
         public ActionResult AddPost()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult AddPost(SendMail.Models.MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("newsletter.hr.raast@gmail.com", "cd9#M6Y3tnaS$p$R$tSP"); // Enter seders User name and password  
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("AddPost", _objModelMail);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult Verify()
---
+++ RAAST_web/Models/MailModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RAAST_web.Models
+{
+    public class MailModel
+    {
+        public string From
+        {
+            get;
+            set;
+        }
+        public string To
+        {
+            get;
+            set;
+        }
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string Body
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
