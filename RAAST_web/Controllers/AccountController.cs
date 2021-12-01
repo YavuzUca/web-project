d8b17c2ecf654a60b76312aa496156254eb6f72f
Yannick <yannickvreede@gmail.com>
2021-12-01 22:54:35 +0100

    Nieuwe editor aanmaken logt niet meer automatisch in
    Code cleanup moet nog gedaan worden, nog veel onnodige files

--- RAAST_web/Controllers/AccountController.cs
+++ RAAST_web/Controllers/AccountController.cs
@@ -153,19 +153,24 @@ namespace RAAST_web.Controllers
             {
                 var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
+                
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    /*
+                    //await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
+                    */
+                    return RedirectToAction("Index", "AspNetUsers");
+                    
                 }
+                
                 AddErrors(result);
+                
             }
 
             // If we got this far, something failed, redisplay form
--- RAAST_web/Controllers/ManageController.cs
+++ RAAST_web/Controllers/ManageController.cs
@@ -6,9 +6,9 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using TESTAUTH.Models;
+using RAAST_web.Models;
 
-namespace TESTAUTH.Controllers
+namespace  RAAST_web.Controllers
 {
     [Authorize]
     public class ManageController : Controller
