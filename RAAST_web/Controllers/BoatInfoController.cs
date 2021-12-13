547db852106ecd99927d03761c49212b75905e89
Yavuz Uca <yavuz_uca@hotmail.com>
2021-12-13 20:27:51 +0100

    eerste stap API

---
+++ RAAST_web/Controllers/BoatInfoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Http.Description;
+using RAAST_web.Models;
+using RAAST_web.App_Start;
+
+namespace RAAST_web.Controllers
+{
+    public class BoatInfoController : ApiController
+    {
+        private string url = "https://transceiver.hr.nl/api/rock7s";
+
+        // GET: api/BoatInfo
+        public async Task<string> GetBoatInfo()
+        {
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                try
+                {
+                    var jsonlist = await response.Content.ReadAsStringAsync();
+                    return jsonlist;
+                }
+                catch
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
